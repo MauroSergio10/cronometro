68f4e80558d81a890d9b62ab82c128cdc4b435fa
Mauro Sérgio <maurosergiocantuaria@gmail.com>
2022-08-10 13:11:48 -0300

    edição do documento cronometro/index.js

--- src/cronometro/index.js
+++ src/cronometro/index.js
@@ -1,16 +1,52 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
+
+const initialState = {
+  hours: 0,
+  minute: 0,
+  seconds: 0  
+}
 
 const Cronometro = () => {
-    const [count, setCount] = useState(0);
+  const [time, setTime] = useState(initialState) 
 
-    useEffect(() => {
-      setInterval(() => {
-        setCount(prevCount => prevCount + 1);
-      }, 1000);
-    }, []);
-    
-  
-    return <h1>The component has been rendered for {count} seconds</h1>;
+  const handleClick = (e) => {
+    setInterval(() => {
+
+      var updateMinute = {}
+      var updateSeconds = {}
+      var seconds = (e.target.value)
+      
+      if (seconds > 5){
+        seconds = 0;  
+        updateMinute = {minute: time.minute + 1}
+        updateSeconds = {seconds: 0}
+        e.target = 0
+        setTime(time => ({
+          ...time,
+          ...updateMinute,
+          ...updateSeconds
+        }));
+      }else{ 
+        updateSeconds = {seconds: e.target.value++}
+        setTime(time => ({
+          ...time,
+          ...updateSeconds
+        }));
+      }
+    },1000);
+  }
+
+  /*setInterval(() => {
+    setTime(time.times.seconds + 1)
+  }, 1000)}*/
+
+  const textoTime = time.hours + ":" + time.minute + ":" + time.seconds;
+  return(
+    <div>
+      <h1>{textoTime}</h1>
+      <button type="button" onClick={e => handleClick(e)}>Start</button>
+    </div>
+  )
 
 }
 
